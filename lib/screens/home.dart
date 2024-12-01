19a7f904d89b38006f84e1a4695360fe84c06096
Zamir Florentino <zamirflorentino@Zamirs-MacBook-P.local>
2024-12-01 00:07:45 -0400

    change back ground color

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:namer_app/provider/paymet_provider.dart';
 import 'package:namer_app/screens/add_payment.dart';
 import 'package:namer_app/screens/manage_services.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:flutter/cupertino.dart';
 
 
@@ -21,72 +23,109 @@ paymentProvder =  Provider.of<PaymentProvder>(context, listen: false);
 
     super.initState();
   }
+ getservices () async{
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
 
+
+ }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        actions: [
+      
+
+      
+      body: DecoratedBox (
+
+
+
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin:Alignment.topCenter,
+          colors: [Colors.blue,Colors.red])
+
+
+        ) ,
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+
+              crossAxisAlignment: CrossAxisAlignment.start,
+             
+
+              children: [
+                 Row(
+                   children: [
+                    Spacer(),
+                    IconButton(
+                                 icon: Icon(Icons.add_business),
+                                 color: Colors.white70,
+                                 iconSize: 35,
+                                 
+                               
+                                 
+                                 onPressed: () {
+                                   Navigator.push(context, MaterialPageRoute(builder: (context) => ManageServicesScreen())); // Navega a la pantalla de administración de servicios
+                         
+                                   // Navegar a la pantalla de Configuración
+                                 },
+                               ),
+                   ],
+                 ),
+                // Sección de Servicios Pendientes
+                Text(
+                  "Servicios Pendientes de Pago",
+                 
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                   
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  
+                  child: Scrollbar(
+
+                    child: ListView.builder(
+                                    
+                    itemCount:paymentProvder.serviceList.length ,
+                    itemBuilder:(context,index){
+                    final payment = paymentProvder.serviceList[index];
+                        String formatdate =  DateFormat('yyyy-MM-dd').format(payment.dueDate);
+                              
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          child: PaymentCard(provider: payment.provider, amount: payment.amount, paymentDate: formatdate, note: ""));
+                      } ,
+                                    
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
           
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ManageServicesScreen())); // Navega a la pantalla de administración de servicios
-    
-              // Navegar a la pantalla de Configuración
-            },
+                // Sección de Pagos Recientes
+                Text(
+                  "Pagos Recientes",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: ListView(
+                    // children: [
+                    //   PaymentCard(
+                    //     provider: "Internet",
+                    //     amount: 50.0,
+                    //     paymentDate: "10 de octubre",
+                    //   ),
+                    //   PaymentCard(
+                    //     provider: "Teléfono",
+                    //     amount: 20.0,
+                    //     paymentDate: "9 de octubre",
+                    //   ),
+                    //   // Añadir más pagos como se requiera
+                    // ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Sección de Servicios Pendientes
-            Text(
-              "Servicios Pendientes de Pago",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount:paymentProvder.serviceList.length ,
-                itemBuilder:(context,index){
-final payment = paymentProvder.serviceList[index];
-
-                  return PaymentCard(provider: payment.provider, amount: payment.amount, paymentDate: payment.dueDate.toString(), note: "");
-                } ,
-            
-              ),
-            ),
-            SizedBox(height: 20),
-
-            // Sección de Pagos Recientes
-            Text(
-              "Pagos Recientes",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: ListView(
-                // children: [
-                //   PaymentCard(
-                //     provider: "Internet",
-                //     amount: 50.0,
-                //     paymentDate: "10 de octubre",
-                //   ),
-                //   PaymentCard(
-                //     provider: "Teléfono",
-                //     amount: 20.0,
-                //     paymentDate: "9 de octubre",
-                //   ),
-                //   // Añadir más pagos como se requiera
-                // ],
-              ),
-            ),
-          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -148,13 +187,16 @@ class PaymentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    
+    return  Card(
+      margin: EdgeInsets.zero,
       color: Colors.blueAccent.shade100,
       child: ListTile(
-        title: Text(provider),
-        subtitle: Text("Fecha de Pago: $paymentDate"),
-        trailing: Text("\$$amount",
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      title: Text(provider),
+      subtitle: Text("Fecha de Pago: $paymentDate"),
+      trailing: Text("\$$amount",
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+      
       ),
     );
   }
